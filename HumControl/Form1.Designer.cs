6b03c458f7efb100e67a15442bf506074ab5ecef
BloodDecember <egar@list.ru>
2017-10-08 12:54:26 +0300

    0810

---
+++ HumControl/Form1.Designer.cs
@@ -0,0 +1,168 @@
+﻿namespace HumControl
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.checkBox1 = new System.Windows.Forms.CheckBox();
+            this.button2 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(108, 33);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(100, 20);
+            this.textBox1.TabIndex = 0;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(6, 36);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(96, 13);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Мин.   Влажность";
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(108, 6);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(100, 20);
+            this.textBox2.TabIndex = 2;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(6, 9);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(96, 13);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "Макс. Влажность";
+            // 
+            // label3
+            // 
+            this.label3.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.label3.Font = new System.Drawing.Font("Impact", 20.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label3.Location = new System.Drawing.Point(215, 219);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(57, 34);
+            this.label3.TabIndex = 4;
+            // 
+            // label4
+            // 
+            this.label4.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.label4.Font = new System.Drawing.Font("Impact", 20.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label4.Location = new System.Drawing.Point(12, 219);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(57, 34);
+            this.label4.TabIndex = 5;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(85, 202);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(110, 51);
+            this.button1.TabIndex = 6;
+            this.button1.Text = "Обновить";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // timer1
+            // 
+            this.timer1.Interval = 10000;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // checkBox1
+            // 
+            this.checkBox1.AutoSize = true;
+            this.checkBox1.Location = new System.Drawing.Point(12, 179);
+            this.checkBox1.Name = "checkBox1";
+            this.checkBox1.Size = new System.Drawing.Size(76, 17);
+            this.checkBox1.TabIndex = 7;
+            this.checkBox1.Text = "Авто.Обн.";
+            this.checkBox1.UseVisualStyleBackColor = true;
+            this.checkBox1.CheckedChanged += new System.EventHandler(this.checkBox1_CheckedChanged);
+            // 
+            // button2
+            // 
+            this.button2.Font = new System.Drawing.Font("Microsoft Sans Serif", 6.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.button2.Location = new System.Drawing.Point(214, 6);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(58, 47);
+            this.button2.TabIndex = 8;
+            this.button2.Text = "Обновить";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(284, 262);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.checkBox1);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textBox1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedToolWindow;
+            this.Name = "Form1";
+            this.RightToLeft = System.Windows.Forms.RightToLeft.No;
+            this.Text = "Микроклимат";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.CheckBox checkBox1;
+        private System.Windows.Forms.Button button2;
+        public System.Windows.Forms.Label label3;
+    }
+}
+
---
+++ HumControl/Form1.cs
@@ -0,0 +1,114 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Net.Sockets;
+using System.IO;
+
+namespace HumControl
+{
+
+    public partial class Form1 : Form
+    {
+        const int port = 80;
+        const string address = "192.168.1.101";
+        private static readonly string ClientRequestStringH = "H";
+        private static readonly byte[] ClientRequestBytesH = Encoding.ASCII.GetBytes(ClientRequestStringH);
+        private static readonly string ClientRequestStringT = "T";
+        private static readonly byte[] ClientRequestBytesT = Encoding.ASCII.GetBytes(ClientRequestStringT);
+        private static readonly string ClientRequestStringMAX = "MAX";
+        private static readonly byte[] ClientRequestBytesMAX = Encoding.ASCII.GetBytes(ClientRequestStringMAX);
+        private static readonly string ClientRequestStringMIN = "MIN";
+        private static readonly byte[] ClientRequestBytesMIN = Encoding.ASCII.GetBytes(ClientRequestStringMIN);
+
+
+        public Form1()
+        {
+            InitializeComponent();
+            ConnectAsTcpClient(ClientRequestBytesH, label3);
+            ConnectAsTcpClient(ClientRequestBytesT, label4);
+            ConnectAsTcpClient(ClientRequestBytesMAX, textBox2);
+            ConnectAsTcpClient(ClientRequestBytesMIN, textBox1);
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            ConnectAsTcpClient(ClientRequestBytesH, label3);
+            ConnectAsTcpClient(ClientRequestBytesT, label4);
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            ConnectAsTcpClient(ClientRequestBytesH, label3);
+            ConnectAsTcpClient(ClientRequestBytesT, label4);
+        }
+
+        private void checkBox1_CheckedChanged(object sender, EventArgs e)
+        {
+            timer1.Enabled = true;
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            string HMAI = textBox2.Text + textBox1.Text + "HMAI";
+            byte[] HMAIB = Encoding.ASCII.GetBytes(HMAI);
+            ConnectAsTcpClient(HMAIB);
+            ConnectAsTcpClient(ClientRequestBytesMAX, textBox2);
+            ConnectAsTcpClient(ClientRequestBytesMIN, textBox1);
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            
+        }
+
+        public async void ConnectAsTcpClient(byte[] CR, Label label)
+        {
+            using (var tcpClient = new TcpClient())
+            {
+                await tcpClient.ConnectAsync("192.168.1.101", 80);
+                using (var networkStream = tcpClient.GetStream())
+                {
+                    await networkStream.WriteAsync(CR, 0, CR.Length);
+
+                    var buffer = new byte[4096];
+                    var byteCount = await networkStream.ReadAsync(buffer, 0, buffer.Length);
+                    var response = Encoding.ASCII.GetString(buffer, 0, byteCount);
+                    label.Text = response;
+                }
+            }
+        }
+        public async void ConnectAsTcpClient(byte[] CR, TextBox textBox)
+        {
+            using (var tcpClient = new TcpClient())
+            {
+                await tcpClient.ConnectAsync("192.168.1.101", 80);
+                using (var networkStream = tcpClient.GetStream())
+                {
+                    await networkStream.WriteAsync(CR, 0, CR.Length);
+
+                    var buffer = new byte[4096];
+                    var byteCount = await networkStream.ReadAsync(buffer, 0, buffer.Length);
+                    var response = Encoding.ASCII.GetString(buffer, 0, byteCount);
+                    textBox.Text = response;
+                }
+            }
+        }
+        public async void ConnectAsTcpClient(byte[] CR)
+        {
+            using (var tcpClient = new TcpClient())
+            {
+                await tcpClient.ConnectAsync("192.168.1.101", 80);
+                using (var networkStream = tcpClient.GetStream())
+                {
+                    await networkStream.WriteAsync(CR, 0, CR.Length);
+                }
+            }
+        }
+    }
+}
